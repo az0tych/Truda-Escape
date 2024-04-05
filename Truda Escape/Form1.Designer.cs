0b96246e810313d33cf50a1f16c88775a972f767
hochukvass <hochukvass@DESKTOP-2IGUUQ4>
2024-04-05 09:05:51 +0300

    Добавьте файлы проекта.

---
+++ Truda Escape/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿using System.Drawing;
+
+namespace Truda_Escape
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.left = new System.Windows.Forms.Button();
+            this.right = new System.Windows.Forms.Button();
+            this.front_p = new System.Windows.Forms.PictureBox();
+            this.menu = new System.Windows.Forms.Button();
+            this.inventory = new System.Windows.Forms.GroupBox();
+            this.i5 = new System.Windows.Forms.PictureBox();
+            this.i4 = new System.Windows.Forms.PictureBox();
+            this.i3 = new System.Windows.Forms.PictureBox();
+            this.i2 = new System.Windows.Forms.PictureBox();
+            this.i1 = new System.Windows.Forms.PictureBox();
+            this.kod = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.front_p)).BeginInit();
+            this.inventory.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.i5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // left
+            // 
+            this.left.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.left.Location = new System.Drawing.Point(12, 560);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(75, 75);
+            this.left.TabIndex = 0;
+            this.left.Text = "◀";
+            this.left.UseVisualStyleBackColor = true;
+            this.left.Click += new System.EventHandler(this.left_Click);
+            // 
+            // right
+            // 
+            this.right.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.right.Location = new System.Drawing.Point(1833, 560);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(75, 75);
+            this.right.TabIndex = 1;
+            this.right.Text = "▶";
+            this.right.UseVisualStyleBackColor = true;
+            this.right.Click += new System.EventHandler(this.right_Click);
+            // 
+            // front_p
+            // 
+            this.front_p.BackColor = System.Drawing.Color.Transparent;
+            this.front_p.Image = global::Truda_Escape.Properties.Resources.перед_сток;
+            this.front_p.Location = new System.Drawing.Point(0, 0);
+            this.front_p.Name = "front_p";
+            this.front_p.Size = new System.Drawing.Size(1920, 1080);
+            this.front_p.TabIndex = 2;
+            this.front_p.TabStop = false;
+            this.front_p.MouseClick += new System.Windows.Forms.MouseEventHandler(this.front_p_MouseClick);
+            // 
+            // menu
+            // 
+            this.menu.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.menu.Location = new System.Drawing.Point(12, 12);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(75, 75);
+            this.menu.TabIndex = 3;
+            this.menu.Text = "≡";
+            this.menu.UseVisualStyleBackColor = true;
+            this.menu.Click += new System.EventHandler(this.menu_Click);
+            // 
+            // inventory
+            // 
+            this.inventory.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.inventory.BackColor = System.Drawing.Color.Transparent;
+            this.inventory.BackgroundImage = global::Truda_Escape.Properties.Resources.inventory;
+            this.inventory.Controls.Add(this.i5);
+            this.inventory.Controls.Add(this.i4);
+            this.inventory.Controls.Add(this.i3);
+            this.inventory.Controls.Add(this.i2);
+            this.inventory.Controls.Add(this.i1);
+            this.inventory.Location = new System.Drawing.Point(629, -38);
+            this.inventory.Name = "inventory";
+            this.inventory.Size = new System.Drawing.Size(664, 172);
+            this.inventory.TabIndex = 4;
+            this.inventory.TabStop = false;
+            this.inventory.Text = " ";
+            // 
+            // i5
+            // 
+            this.i5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.i5.Location = new System.Drawing.Point(525, 45);
+            this.i5.Name = "i5";
+            this.i5.Size = new System.Drawing.Size(107, 107);
+            this.i5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.i5.TabIndex = 4;
+            this.i5.TabStop = false;
+            this.i5.Click += new System.EventHandler(this.i_Click);
+            // 
+            // i4
+            // 
+            this.i4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.i4.Location = new System.Drawing.Point(402, 45);
+            this.i4.Name = "i4";
+            this.i4.Size = new System.Drawing.Size(107, 107);
+            this.i4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.i4.TabIndex = 3;
+            this.i4.TabStop = false;
+            this.i4.Click += new System.EventHandler(this.i_Click);
+            // 
+            // i3
+            // 
+            this.i3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.i3.Location = new System.Drawing.Point(278, 45);
+            this.i3.Name = "i3";
+            this.i3.Size = new System.Drawing.Size(107, 107);
+            this.i3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.i3.TabIndex = 2;
+            this.i3.TabStop = false;
+            this.i3.Click += new System.EventHandler(this.i_Click);
+            // 
+            // i2
+            // 
+            this.i2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.i2.Location = new System.Drawing.Point(155, 44);
+            this.i2.Name = "i2";
+            this.i2.Size = new System.Drawing.Size(107, 107);
+            this.i2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.i2.TabIndex = 1;
+            this.i2.TabStop = false;
+            this.i2.Click += new System.EventHandler(this.i_Click);
+            // 
+            // i1
+            // 
+            this.i1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.i1.Location = new System.Drawing.Point(32, 44);
+            this.i1.Name = "i1";
+            this.i1.Size = new System.Drawing.Size(107, 107);
+            this.i1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.i1.TabIndex = 0;
+            this.i1.TabStop = false;
+            this.i1.Click += new System.EventHandler(this.i_Click);
+            // 
+            // kod
+            // 
+            this.kod.AutoSize = true;
+            this.kod.BackColor = System.Drawing.Color.White;
+            this.kod.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.kod.Location = new System.Drawing.Point(571, 663);
+            this.kod.Name = "kod";
+            this.kod.Size = new System.Drawing.Size(0, 73);
+            this.kod.TabIndex = 5;
+            this.kod.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(21F, 39F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::Truda_Escape.Properties.Resources.перед_фон;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.kod);
+            this.Controls.Add(this.inventory);
+            this.Controls.Add(this.menu);
+            this.Controls.Add(this.right);
+            this.Controls.Add(this.left);
+            this.Controls.Add(this.front_p);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "Form1";
+            this.Text = "Truda Escape";
+            ((System.ComponentModel.ISupportInitialize)(this.front_p)).EndInit();
+            this.inventory.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.i5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button left;
+        private System.Windows.Forms.Button right;
+        private System.Windows.Forms.PictureBox front_p;
+        private System.Windows.Forms.Button menu;
+        private System.Windows.Forms.GroupBox inventory;
+        private System.Windows.Forms.PictureBox i1;
+        private System.Windows.Forms.PictureBox i2;
+        private System.Windows.Forms.PictureBox i5;
+        private System.Windows.Forms.PictureBox i4;
+        private System.Windows.Forms.PictureBox i3;
+        private System.Windows.Forms.Label kod;
+    }
+}
+
---
+++ Truda Escape/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace Truda_Escape
+{
+    public partial class Form1 : Form
+    {
+        //SoundPlayer S_disk = new SoundPlayer(Properties.Resources.дисковод);
+        Random random = new Random();
+        int side = 0, back = 0, right_d = 0, right_m = 0, left_ = 0;
+        Image back_im = Properties.Resources.зад_закрытые_шторы_магнит,
+            right_im = Properties.Resources.право_диск_лист, 
+            left_im = Properties.Resources.лево_пк_open,
+            front_im = Properties.Resources.перед_сток,
+            mT = Properties.Resources.магнит_тр , 
+            mK = Properties.Resources.магнит_кв, mZ = Properties.Resources.магнит_зв, disk = Properties.Resources.диск;
+        string kodik, i_che = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            kodik = $"{random.Next(9)}{random.Next(9)}{random.Next(9)}{random.Next(9)}";
+            kod.Text = kodik;
+            mT.Tag = "mT"; mK.Tag = "mK"; mZ.Tag = "mZ"; disk.Tag = "disk";
+        }
+
+        private void front_add()
+        {
+            side = 0;
+            this.BackgroundImage = Properties.Resources.перед_фон;
+            front_p.Image = front_im;
+        }
+
+        private void right_add()
+        {
+            side = 3;
+            this.BackgroundImage = Properties.Resources.право_фон;
+            front_p.Image = right_im;
+        }
+
+        private void back_add()
+        {
+            side = 2;
+            this.BackgroundImage = Properties.Resources.зад_фон;
+            front_p.Image = back_im;
+        }
+
+        private async void left_add()
+        {
+            side = 1;
+            this.BackgroundImage = Properties.Resources.лево_фон;
+            front_p.Image = left_im;
+            if (left_ == 1)
+            {
+                await Task.Delay(400);
+                kod.Visible = true;
+            }
+        }
+
+
+        private void i_Click(object sender, EventArgs e)
+        {
+            i_che = ((PictureBox)sender).Image.Tag.ToString();
+        }
+
+        private void inventory_add(Image im)
+        {
+            for (int i = inventory.Controls.Count - 1; i >= 0; i--)
+            {
+                if (((PictureBox)inventory.Controls[i]).Image == null)
+                {
+                    ((PictureBox)inventory.Controls[i]).Image = im;
+                    break;
+                }
+            }
+        }
+
+        private void inventory_und(Image im)
+        {
+            for (int i = inventory.Controls.Count - 1; i >= 0; i--)
+            {
+                if (((PictureBox)inventory.Controls[i]).Image.Tag == im.Tag)
+                {
+                    ((PictureBox)inventory.Controls[i]).Image = null;
+                    break;
+                }
+            }
+        }
+
+        private void front_ch()
+        {
+            
+        }
+
+        private void right_ch(int X, int Y)
+        {
+            if (Y > 570 & Y < 670 & X > 310 & X < 465)
+            {
+                if (right_d == 0 & right_m == 0)
+                {
+                    right_im = Properties.Resources.право_лист;
+                    right_d = 1;
+                    inventory_add(disk);
+                }
+                else if (right_d == 0 & right_m == 1)
+                {
+                    right_im = Properties.Resources.право_магнит;
+                    right_d = 1;
+                    inventory_add(disk);
+                }
+                if (right_d == 0 & right_m == 2)
+                {
+                    right_im = null;
+                    right_d = 1;
+                    inventory_add(disk);
+                }
+            }
+            if (Y > 820 & Y < 920 & X > 1030 & X < 1178)
+            {
+                if(right_d == 0 & right_m == 0)
+                {
+                    right_im = Properties.Resources.право_диск_магнит;
+                    right_m = 1;
+                }
+                else if(right_d == 0 & right_m == 1)
+                {
+                    right_im = Properties.Resources.право_диск;
+                    right_m = 2;
+                    inventory_add(mK);
+                }
+
+                else if(right_d == 1 & right_m == 0)
+                {
+                    right_im = Properties.Resources.право_магнит;
+                    right_m = 1;
+                }
+                else if(right_d == 1 & right_m == 1)
+                {
+                    right_im = null;
+                    right_m = 2;
+                    inventory_add(mK);
+                }
+            }
+            front_p.Image = right_im;
+        }
+
+        private void back_ch(int X, int Y)
+        {
+            if (back == 0 & Y > 140 & Y < 870)
+            {
+                back_im = Properties.Resources.зад_открфтые_шторы_магнит;
+                back = 1;
+            }
+            else if (back == 1 & Y > 550 & Y < 770 & X > 160 & X < 320)
+            {
+                back_im = Properties.Resources.зад_открфтые_шторы;
+                back = 2;
+                inventory_add(mZ);
+            }
+            front_p.Image = back_im;
+        }
+
+        private async void left_ch(int X, int Y)
+        {
+            if (i_che == disk.Tag.ToString() & left_ == 0 & Y > 930 & Y < 1080 & X > 400 & X < 595)
+            {
+                left_im = Properties.Resources.лево_пк_close;
+                left_ = 1;
+                inventory_und(disk);
+                await Task.Delay(200);
+                kod.Visible = true;
+            }
+            front_p.Image = left_im;
+        }
+
+        private void playSound()
+        {
+            SoundPlayer Sound = new SoundPlayer();
+            Sound.Play();
+        }
+
+        private void left_Click(object sender, EventArgs e)
+        {
+            kod.Visible = false;
+            switch (side)
+            {
+                case 0: left_add(); break;
+                case 1: back_add(); break;
+                case 2: right_add(); break;
+                case 3: front_add(); break;
+            }
+        }
+
+        private void right_Click(object sender, EventArgs e)
+        {
+            kod.Visible = false;
+            switch (side)
+            {
+                case 0: right_add(); break;
+                case 1: front_add(); break;
+                case 2: left_add(); break;
+                case 3: back_add(); break;
+            }
+            i_che = "";
+        }
+
+        private void menu_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void front_p_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch (side)
+            {
+                case 0: ; break;
+                case 1: left_ch(e.X, e.Y); break;
+                case 2: back_ch(e.X, e.Y); 
+                    break;
+                case 3: right_ch(e.X, e.Y); break;
+            }
+            i_che = "";
+        }
+    }
+}
